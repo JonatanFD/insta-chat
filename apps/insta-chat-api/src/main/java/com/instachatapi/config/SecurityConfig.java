c9920cb81bbe26125fea37cd94ae075117ffdf9e
JonatanFD <u20211b995@upc.edu.pe>
2026-03-08 01:05:52 -0500

    feat: fix cors

--- apps/insta-chat-api/src/main/java/com/instachatapi/config/SecurityConfig.java
+++ apps/insta-chat-api/src/main/java/com/instachatapi/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -32,5 +37,19 @@ public class SecurityConfig {
                     .authenticated()
             )
             .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        var config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://192.168.0.33:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
