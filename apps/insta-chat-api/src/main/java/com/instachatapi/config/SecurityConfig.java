9bbf7a5371ad068f26fc3e962aa2c746de4dd5ba
JonatanFD <u20211b995@upc.edu.pe>
2026-03-12 21:13:21 -0500

    feat: fix cors

--- apps/insta-chat-api/src/main/java/com/instachatapi/config/SecurityConfig.java
+++ apps/insta-chat-api/src/main/java/com/instachatapi/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.instachatapi.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,8 +13,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -24,31 +23,43 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(
+        ServerHttpSecurity http
+    ) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(Customizer.withDefaults())
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .authorizeExchange(exchanges ->
-                        exchanges
-                                .pathMatchers(
-                                        "/api/chats/**",
-                                        "/ws/chats/**",
-                                        "/v3/api-docs/**",
-                                        "/swagger-ui/**",
-                                        "/swagger-ui.html"
-                                ).permitAll()
-                                .anyExchange().permitAll()
-                )
-                .build();
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .cors(Customizer.withDefaults())
+            .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+            .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+            .authorizeExchange(exchanges ->
+                exchanges
+                    .pathMatchers(
+                        "/api/chats/**",
+                        "/ws/chats/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html"
+                    )
+                    .permitAll()
+                    .anyExchange()
+                    .permitAll()
+            )
+            .build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         var config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://192.168.0.33:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(
+            List.of(
+                "http://localhost:5173",
+                "http://192.168.0.33:5173",
+                "https://instachatapp.vercel.app"
+            )
+        );
+        config.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        );
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
